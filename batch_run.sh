aa0be17e57345d14f50ff8dae91cd8069ff6bd30
bokae <e.bokanyi@gmail.com>
2018-11-21 15:28:37 +0100

    Small batch to check.

--- batch_run.sh
+++ batch_run.sh
@@ -8,7 +8,7 @@
 
 # more fancy: only runs left out pieces
 #for conf in `cat slurm-* | grep run | cut -d " " -f5 | sed 's/\.//g' | sort -n | comm -3 - configs/0831_all.list`
-for file in `ls configs/0831_base*baseline*nearest*geom0* | grep -v 1600 | grep -v 6400`
+for file in `ls configs/1114_base*d_190_*geom_0*_true*`
 do
 	conf=`echo $file | cut -d "/" -f2 | sed 's/.conf//g'`
 	echo "Submitting "$conf"..."
