d7c4dd075e2f8d86da5c52335476a12432e7d04f
bokae <bokanyi@complex.elte.hu>
2018-11-21 14:33:29 +0100

    Preparation check.

--- visualize.py
+++ visualize.py
@@ -225,9 +225,7 @@ class ResultParser:
 
             # unioning all data from all timestamps, needed for global range min max values
             # for every metrics except for timestamp
-            print(res.index.map(lambda x: x != 'timestamp'))
-            print(res[res.index.map(lambda x: x != 'timestamp')].index.tolist())
-            res_all = res[res.index.map(lambda x: x != 'timestamp')] \
+            res_all = res[res.index.map(lambda x: x != 'timestamp').tolist()] \
                 .apply(lambda row: [e for l in row.tolist() for e in l], axis=1)
 
             # minima and maxima
