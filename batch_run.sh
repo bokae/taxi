5da0902422d994c007ebaae9af3d5ebfc602121a
bokae <e.bokanyi@gmail.com>
2019-02-14 16:08:00 +0100

    Fixed ls argument list too long in batch_run.

--- batch_run.sh
+++ batch_run.sh
@@ -8,9 +8,29 @@
 
 # more fancy: only runs left out pieces
 #for conf in `cat slurm-* | grep run | cut -d " " -f5 | sed 's/\.//g' | sort -n | comm -3 - configs/0831_all.list`
-for file in `ls configs/1114_base*d_190_*geom_0*_true*`
+#for file in `ls configs/1114_base*_true*`
+
+#for file in `ls configs/1114_base*_stay*home*_false*`
+#do
+#	conf=`echo $file | cut -d "/" -f2 | sed 's/.conf//g'`
+#	echo "Submitting "$conf"..."
+#	sbatch -c 1 --mem=1000 ./run.py $conf
+#done
+
+#for file in `ls configs/1114_base*_stay*base*_false*`
+#do
+#	conf=`echo $file | cut -d "/" -f2 | sed 's/.conf//g'`
+#	echo "Submitting "$conf"..."
+#	sbatch -c 1 --mem=1000 ./run.py $conf
+#done
+
+for alg in "random_limited" "nearest" "poorest" "random_unlimited"
 do
-	conf=`echo $file | cut -d "/" -f2 | sed 's/.conf//g'`
-	echo "Submitting "$conf"..."
-	sbatch -c 1 --mem=1000 ./run.py $conf
+	for file in `ls configs/*$alg*`
+	do
+		conf=`echo $file | cut -d "/" -f2 | sed 's/.conf//g'`
+		echo "Submitting "$conf"..."
+		sbatch -c 1 --mem=1000 ./run.py $conf
+		sleep 1
+	done
 done
